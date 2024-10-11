1cb8560363ab0fbb5696e18d053fd5bc0e2407cc
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-10-11 07:19:26 +0600

    first commit

---
+++ scope.js
@@ -0,0 +1,17 @@
+// globls scope
+// functional scope
+// block scope
+
+const x = 5 
+
+// console.log(x)
+
+// if (true) {
+//     console.log(x)
+// }
+
+function show(){
+    console.log(x)
+}
+
+show()
