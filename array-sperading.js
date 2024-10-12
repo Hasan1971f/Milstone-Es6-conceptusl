83f0688ff148faad3e8b74c40d669ca66b1f3697
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-10-12 09:30:07 +0600

    added a array defututring

--- array-sperading.js
+++ array-sperading.js
@@ -1,6 +1,6 @@
-const arr1 = [1, 2, 3]
-const arr2 = [...arr1]
-// console.log(arr2)
+// const arr1 = [1, 2, 3]
+// const arr2 = [...arr1]
+// // console.log(arr2)
 
 // const arr2 =[]
 // for (let item of arr1) {
@@ -16,12 +16,24 @@ const arr2 = [...arr1]
 // console.log(arr2)
 // console.log(arr1)
 
-const arr3 =  [20, 30, 40]
-const arr4 =  [...arr3]
+// const arr3 =  [20, 30, 40]
+// const arr4 =  [...arr3]
 
-arr4.push(70)
-arr3.push(80)
+// arr4.push(70)
+// arr3.push(80)
 
-console.log(arr3)
+// console.log(arr3)
 
 
+// Array defacturing.js
+
+const names = ["Rony", "Rajib", "loman", "Hasn"]
+
+// const name2 = names[1]
+// const name3 = names[2]
+
+// console.log(name2)
+
+const [_, , d] = ["Rony", "Rajib", "loman", "Hasn"]
+// console.log(a, b, c, d)
+console.log( d)
