7bcbd3a7a79752b9b5bd196b0f507975d5bbc3fc
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-10-12 07:41:09 +0600

    added a arrow.js

---
+++ arrow.js
@@ -0,0 +1,36 @@
+// function info(){
+//     return "ke chai..."
+// }
+
+const info = () => "what is this..."
+   
+// const result = info()
+// console.log(info())
+
+// function man(s ,m){
+//     return s + m 
+// }
+// console.log(man(3, 7))
+
+// const man = (s) => s * 2
+// console.log(man(6))
+
+// const add = (x,y) => {
+//     const total = x + y
+//     return total
+// }
+// console.log(add(3, 4))
+
+// const add = (x,y) => x + y
+   
+// console.log(add(3, 4))
+
+// const name = ( x, y, z) => x + y + z
+// console.log(name(3, 6, 9))
+
+// function name(x=0, y=0){
+//     return x + y
+// }
+
+const name = (x=0, y=0) => x + y
+console.log(name(2, 1))
