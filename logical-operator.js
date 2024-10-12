2906da48b2cb4cb9623bb132e5a3bd95239b063c
Hasan <mdhasanurrahmanhasan101@gmail.com>
2024-10-12 08:25:49 +0600

    added a logical-operator.js

---
+++ logical-operator.js
@@ -0,0 +1,16 @@
+//  logical operator 2 rokomer 
+// 1. And operator (&&)
+// 2. Or operator (||)
+
+// const x = 5
+// console.log(!!x)
+
+// const money = true
+
+// console.log(money && 'versity jabo')
+
+// ||
+const bus = ""
+const rickshaw = "rickshaw ase"
+
+console.log(bus || rickshaw  )
